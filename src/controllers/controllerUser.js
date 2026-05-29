8d6f8a639201ebaa042c4f6a9ae62b655f5cfca4
jdanielf <danielljd@hotmail.com>
2026-05-29 15:10:33 -0300

    bcrypt

--- src/controllers/controllerUser.js
+++ src/controllers/controllerUser.js
@@ -3,29 +3,33 @@ import crypto from 'crypto'
 import path from 'path'
 import bcrypt from 'bcrypt'
 
-const listarUsuarios = async (req, res) => {
+export const listarUsuarios = async (req, res) => {
     try{
-  const usuarios = await User.findAll()
-  if(!usuarios)return res.status(404).json({message: 'Nenhum usuário encontrado'})
-  
-    res.status(200).json(usuarios)
-    
-      
-   
-    }  catch (error) {
-        console.error('Erro ao listar usuários:', error)
-        res.status(500).json({message: 'Erro ao listar usuários'})
+        const usuarios = await User.findAll()
+        if(!usuarios) return res.status(400).json({mensagem: 'Não tem usuários!'})
+        res.status(200).json(usuarios)
+    }catch(err){
+        res.status(500).json({mensagem: 'Erro no servidor!'})
     }
 }
-export const crudarUsuario = async (req, res) => {
-  
-    const { nome, email, senha } = req.body
-    
-    if(!nome && !email && !senha){
-        return res.status(400).json({message: 'Preencha todos os campos'})
 
-
-
+export const criarUsuario = async (req, res) => {
+    const {nome, email, senha} = req.body
+    if(!nome && !email && !senha) return res.status(400).json({mensagem: 'Preencha todos os campos!'})
+    try{
+        // exemplo com crypto
+        // const senhaCript = crypto.createHash('sha256')
+        // senhaCript.update(senha)
+        // const senhaOK = senhaCript.digest('hex')
+        // console.log(senhaOK)
+        const senhaCript = await bcrypt.hash(senha, 10)
+        await User.create({nome: nome, email: email, senha: senhaCript})
+        res.status(200).json({mensagem: 'Usuário criado com sucesso!'})
+    }catch(err){
+        res.status(500).json({mensagem: 'Erro no servidor!'})
+    }
 }
 
+export const cadastrarUsuario = (req, res) => {
+    res.sendFile(path.resolve('./src/public/html/cadastroUsuario.html'))
 }
--- src/routers/rotaUser.js
+++ src/routers/rotaUser.js
@@ -4,7 +4,7 @@ import { listarUsuarios } from '../controllers/controllerUser'
 const routerUser = exprex.Router()
 
 routerUser.get('/usurio', listarUsuarios)
-routerUser.post('/usurio', crudarUsuario)
-
+routeUser.post('/usuario', criarUsuario)
+routeUser.get('/cadastroUsuario', cadastrarUsuario)
 
 export default routerUser
