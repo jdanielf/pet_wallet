c3d87c4be6eab5c1d9ebf8e70022fff907515248
jdanielf <danielljd@hotmail.com>
2026-06-03 17:09:29 -0300

    att

--- src/controllers/controllerLogin.js
+++ src/controllers/controllerLogin.js
@@ -1,51 +1,96 @@
-import User from '../models/modelUser.js'
-import path from 'path'
-import bcrypt from 'bcrypt'
+        import User from '../models/modelUser.js'
+        import path from 'path'
+        import bcrypt from 'bcrypt'
+          
 
 
 
-export const login = (req, res) => {
-    res.sendFile(path.resolve('./src/public/login.html'))
+
+        export const login = (req, res) => {
+            res.sendFile(path.resolve('./src/public/login.html'))
+        }
+
+
+        export const validarLogin = async (req, res) => {
+            const{email, senha} = req.body
+            if(!email || !senha) return res.status(400).json({mensagem: "Preencha todos os campos!"})
+            try{
+
+                const usuario = await User.findOne({where: {email: email}})
+                if(!usuario) return res.status(400).json({mensagem: "Usuário não encontrado!"})
+                
+        
+            const senhaDescript = await bcrypt.compare(senha, usuario.senha)
+            // console.log(senhaDescript)
+            if(!senhaDescript ) return res.status(400).json({mensagem: "Senha Inválida!"})
+
+                req.session.regenerate((err) =>  {
+                    if(err) return res.status(500).json({mensagem: 'Erro no servidor!'})
+
+                    req.session.usuario = {
+                        id: usuario.idUser,
+                        nome: usuario.nome,
+                        email: usuario.email
+                    }
+
+                    return res.render('index', {usuario: usuario.nome, title: 'Carteira de Pets',
+                        subtitle: 'Registre vacinas, banho, tosa e serviços para cães e gatos'})
+                })
+
+
+            }catch(err){
+                res.status(500).json({mensagem: 'Erro no servidor!'})
+            }
+        }
+
+
+        export const logout = (req, res) => {
+            req.session.destroy((err) => {
+                if(err) return res.status(500).json({mensagem: 'Erro no servidor!'})
+                    res.clearCookie('connect.sid')
+                res.redirect('/login')
+            })
+        }
+
+
+
+
+     
+export const recuperarSenha = (req, res) => {
+    res.sendFile(path.resolve('./src/public/recuperarSenha.html'))
 }
 
 
-export const validarLogin = async (req, res) => {
-    const{email, senha} = req.body
-    if(!email || !senha) return res.status(400).json({mensagem: "Preencha todos os campos!"})
-    try{
-
-        const usuario = await User.findOne({where: {email: email}})
-        if(!usuario) return res.status(400).json({mensagem: "Usuário não encontrado!"})
-         
-   
-    const senhaDescript = await bcrypt.compare(senha, usuario.senha)
-    // console.log(senhaDescript)
-    if(!senhaDescript ) return res.status(400).json({mensagem: "Senha Inválida!"})
-
-        req.session.regenerate((err) =>  {
-            if(err) return res.status(500).json({mensagem: 'Erro no servidor!'})
-
-            req.session.usuario = {
-                id: usuario.idUser,
-                nome: usuario.nome,
-                email: usuario.email
-            }
-
-            return res.render('index', {usuario: usuario.nome, title: 'Carteira de Pets',
-                subtitle: 'Registre vacinas, banho, tosa e serviços para cães e gatos'})
+export const validarEmailRecuperacao = async (req, res) => {
+    const { email } = req.body
+    try {
+        const usuario = await User.findOne({
+            where: { email }
         })
-
-
-    }catch(err){
-        res.status(500).json({mensagem: 'Erro no servidor!'})
+        if (!usuario) {
+            return res.status(400).send('Email não encontrado')
+        }
+        res.redirect(`/novaSenha.html?email=${email}`)
+    } catch (err) {
+        res.status(500).send('Erro no servidor')
     }
 }
 
-
-export const logout = (req, res) => {
-    req.session.destroy((err) => {
-        if(err) return res.status(500).json({mensagem: 'Erro no servidor!'})
-            res.clearCookie('connect.sid')
-        res.redirect('/login')
-    })
+export const novaSenha = async (req, res) => {
+    const { email, senha } = req.body
+    try {
+        const senhaCriptografada =
+            await bcrypt.hash(senha, 10)
+        await User.update(
+            {
+                senha: senhaCriptografada
+            },
+            {
+                where: { email }
+            }
+        )
+        res.send('Senha alterada com sucesso!')
+    } catch (err) {
+        res.status(500).send('Erro ao alterar senha')
+    }
 }
--- src/routers/routerLogin.js
+++ src/routers/routerLogin.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, validarLogin,logout } from '../controllers/controllerLogin.js'
+import { login, validarLogin,logout,recuperarSenha,validarEmailRecuperacao,novaSenha } from '../controllers/controllerLogin.js'
 
 
 const routerLogin = express.Router()
@@ -8,6 +8,8 @@ routerLogin.get('/login', login)
 routerLogin.post('/login', validarLogin)
 routerLogin.post('/logout', logout)
 routerLogin.get('/recuperarSenha',recuperarSenha)
+routerLogin.post('/validarEmailRecuperacao', validarEmailRecuperacao)
+routerLogin.post('/novaSenha', novaSenha)
 
 
 export default routerLogin
