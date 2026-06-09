b5077275c409004eb9da9bff0bfdb959573a0e6a
jdanielf <danielljd@hotmail.com>
2026-06-09 19:24:37 -0300

    henv

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ const __dirname = path.dirname(__filename)
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3000
-const host = process.env.HOST || 'localhost'
+const port = 10000 || 10000
+const host = '0.0.0.0' || 'localhost'
+
+
+// const port = process.env.PORT || 3000
+// const host = process.env.HOST || 'localhost'
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
