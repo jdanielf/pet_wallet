c33b7bb1a66cc63fa39cc0eb9d96d7f159eff975
jdanielf <danielljd@hotmail.com>
2026-06-09 19:48:54 -0300

    att

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.use(routerLogin)
 
 app.get('/', (req, res) => {
   res.render('index', {
+    usuario: 'visitante',
     title: 'Carteira de Pets',
     subtitle: 'Registre vacinas, banho, tosa e serviços para cães e gatos'
   })
