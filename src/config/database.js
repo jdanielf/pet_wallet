7d51db804ae96a612598f5c6f397ebed600a5a2f
jdanielf <danielljd@hotmail.com>
2026-06-10 20:59:55 -0300

    neon

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,36 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, '..', 'database', 'pets.sqlite'),
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: path.join(__dirname, '..', 'database', 'pets.sqlite'),
+//   logging: false
+// })
+
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL,
+  {
+    dialect: 'postgres',
+  dialectOptions: {
+    ssl:{
+      require: true,
+      rejectUnauthorized: false
+
+    }
+
+
+  },
   logging: false
-})
+}
+
+)
 
 export default sequelize
